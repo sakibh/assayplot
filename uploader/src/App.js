176add6970631b678454dba98535679982522207
Sakib Hossain <sakibh97@gmail.com>
2020-08-20 07:57:44 -0400

    Modified and removed unnecessary boilerplate files from React

--- uploader/src/App.js
+++ uploader/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import FileUpload from './components/FileUpload'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><i className="fas fa-chart-line"></i> Assay Plot</h1>
+        <FileUpload />
       </header>
     </div>
   );
